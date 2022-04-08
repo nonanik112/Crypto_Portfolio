e063a3d26dffc3985b73e9a6b2618542cb5f6355
nonanik112 <metehan_1306@hotmail.com>
2022-04-09 01:36:10 +0300

    devam

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  resources :cryptocurrencies
+  resources :cryptostables
   devise_for :users
-  root 'home#index'
-
   get 'home/about'
   get 'home/lookup'
-
-  post'/home/lookup' => 'home/lookup'
+  post 'home/lookup' => 'home/lookup'
+  root 'home#index'
 end
